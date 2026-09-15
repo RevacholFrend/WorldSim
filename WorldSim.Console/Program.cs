0ce0693ea54eefd53f1ac5cd9f23f8359fc3224c
Kim Kitsuragi <revacholfrend@gmail.com>
2026-09-15 22:02:59 +0300

    Выполнены пункты 1.2 и 1.3
    Инициализация мира и тик симуляции(параметры)

--- WorldSim.Console/Program.cs
+++ WorldSim.Console/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+namespace WorldSim.Core.World
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            int seed = 5552465;
+            World world = new World(29, 50);
+            world.Initialize(seed);
+            while (true)
+            {
+                world.Tick();
+                Console.Clear();
+                RenderWorld(world);
+                Thread.Sleep(500);
+            }
+            Console.ReadLine();
+        }
+        static void RenderWorld(World world)
+        {
+            for (int i = 0; i < world.Width; i++)
+            {
+                for (int j = 0; j < world.Height; j++)
+                {
+                    var cell = world.GetCell(i, j);
+                    char c = cell.Terrain switch
+                    {
+                        TerrainType.Water => '~',
+                        TerrainType.Land => ',',
+                        TerrainType.Forest => '#',
+                        TerrainType.Desert => ':',
+                        _=> '?'
+                    };
+                    Console.Write(c);
+
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- WorldSim.Core/World/World.cs
+++ WorldSim.Core/World/World.cs
@@ -18,5 +18,46 @@ namespace WorldSim.Core.World
         }
 
         public ref Cell GetCell(int x, int y) => ref _cells[x, y];
+
+
+
+        public void Initialize(int seed)
+        {
+            var random = new Random(seed);
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    var cell = new Cell();
+                    int roll = random.Next(100);
+                    cell.Terrain = roll switch
+                    {
+                        < 30 => TerrainType.Water,
+                        < 60 => TerrainType.Land,
+                        < 80 => TerrainType.Forest,
+                        _ => TerrainType.Desert
+                    };
+                    cell.Temperature = random.Next(-10, 40);
+                    cell.Humidity = random.Next(0, 100);
+                    cell.Resources = random.Next(0, 100);
+                    _cells[x, y] = cell;
+                }
+            }
+        }
+
+        public void Tick()
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    ref var cell = ref _cells[x, y];
+                    cell.Humidity = Math.Clamp(cell.Humidity + Random.Shared.Next(-2, 3), 0, 100);
+                    cell.Temperature = Math.Clamp(cell.Temperature + Random.Shared.Next(-1, 2), -0,50);
+                }
+            }
+        }
     }
 }
+    
+
